df37ebd5bab42dda7bd951068d0932e3dbebf8b7
Dmitry <bushuevdv@mail.ru>
2023-06-11 00:11:08 +0200

    change paraid

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -100,7 +100,7 @@ pub fn development_config() -> ChainSpec {
                     get_account_id_from_seed::<sr25519::Public>("Eve//stash"),
                     get_account_id_from_seed::<sr25519::Public>("Ferdie//stash"),
                 ],
-                1000.into(),
+                2666.into(),
                 get_account_id_from_seed::<sr25519::Public>("Alice"),
             )
         },
@@ -111,7 +111,7 @@ pub fn development_config() -> ChainSpec {
         None,
         Extensions {
             relay_chain: "rococo-local".into(), // You MUST set this to the correct network!
-            para_id: 1000,
+            para_id: 2666,
         },
     )
 }
@@ -156,7 +156,7 @@ pub fn local_testnet_config() -> ChainSpec {
                     get_account_id_from_seed::<sr25519::Public>("Eve//stash"),
                     get_account_id_from_seed::<sr25519::Public>("Ferdie//stash"),
                 ],
-                1000.into(),
+                2666.into(),
                 get_account_id_from_seed::<sr25519::Public>("Alice"),
             )
         },
@@ -173,7 +173,7 @@ pub fn local_testnet_config() -> ChainSpec {
         // Extensions
         Extensions {
             relay_chain: "rococo-local".into(), // You MUST set this to the correct network!
-            para_id: 1000,
+            para_id: 2666,
         },
     )
 }
